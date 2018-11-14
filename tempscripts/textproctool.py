e0e67b6b1aa2e82baf4b0b12502b68d66a8456f5
Igor_Shevchenko <igorshvch@gmail.com>
2018-11-14 09:58:55 +0300

    Update

--- tempscripts/textproctool.py
+++ tempscripts/textproctool.py
@@ -7,21 +7,24 @@ from debugger import timer
 class IOPickler():
     def __init__(self, file):
         self.file = file
+        self.indexer = []
     
     @timer
-    def write(self, data_iterable, **kwargs):
+    def write(self, data_iterable):
         if self.file.closed:
             return 'File is closed!'
         self.file.seek(0)
-        pick = pickle.Pickler(self.file, **kwargs)
+        self.indexer = []
+        pick = pickle.Pickler(self.file)
         for item in data_iterable:
+            self.indexer.append(self.file.tell())
             pick.dump(item)
     
-    def load_item(self, pos=0, **kwargs):
+    def load_all_item(self, pos=0):
         if self.file.closed:
             return 'File is closed!'
         self.file.seek(pos)
-        unpick = pickle.Unpickler(self.file, **kwargs)
+        unpick = pickle.Unpickler(self.file)
         error = False
         while not error:
             try:
@@ -29,3 +32,26 @@ class IOPickler():
                 yield item
             except:
                 error = True
+    
+    def _load_item(self, pos):
+        if self.file.closed:
+            return 'File is closed!'
+        self.file.seek(pos)
+        unpick = pickle.Unpickler(self.file)
+        return unpick.load()
+    
+    def append(self, item):
+        if self.file.closed:
+            return 'File is closed!'
+        self.file.seek(self.indexer[-1], 2)
+        self.indexer.append(self.file.tell())
+        pick = pickle.Pickler(self.file)
+        pick.dump(item)
+    
+    def __getitem__(self, int_num):
+        try:
+            pos = self.indexer[int_num]
+        except IndexError:
+            return 'Position is out of index'
+        return self._load_item(pos)
+    
