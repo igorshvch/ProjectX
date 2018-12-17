335b66620ceac5fc451712ccddbe98c499f8ece4
Igor Shevchenko <igorshvch@gmail.com>
2018-12-17 13:17:39 +0300

    Update

--- tempscripts/iotext.py
+++ tempscripts/iotext.py
@@ -4,22 +4,25 @@ from debugger import timer
 
 
 class MyReader():
-    def __init__(self):
+    def __init__(self, file):
+        self.__file = file
+        self.__file_size = file.seek(0, 2)
+        self.__state = 0
         self.date_pos = []
         self.dates = {}
-        self.file_size = 0
         self.doc_pos = []
     @timer
-    def find_docs(self, file, pattern_date, pattern_start, pattern_end):
-        file.seek(0)
+    def find_docs(self, pattern_date, pattern_start, pattern_end):
+        self.__file.seek(0)
         last_position = -1
         while True:
-            line = file.readline()
-            current_position = file.tell()
+            line = self.__file.readline()
+            current_position = self.__file.tell()
+            self.__state = current_position/self.__file_size
             if re.match(pattern_date, line):
                 self.date_pos.append(current_position)
                 self.dates.setdefault(
-                    file.readline()[:-1], []
+                    self.__file.readline()[:-1], []
                 ).append(len(self.date_pos)-1)
                 continue
             elif re.match(pattern_start, line):
@@ -29,14 +32,58 @@ class MyReader():
                 end_pos = current_position
                 self.doc_pos.append((start_pos, end_pos))
             if last_position == current_position:
-                self.file_size = round(current_position/(1024**2), 3)
                 break
             else:
                 last_position = current_position
     @timer
-    def find_doc(self, file, index, mode='act', codec='cp1251'):
-        file.seek(0)
-        buffer = file.buffer
+    def find_doc(self, index, mode='act', codec='cp1251'):
+        self.__file.seek(0)
+        buffer = self.__file.buffer
+        if mode == 'act':
+            start, stop = self.doc_pos[index]
+            buffer.seek(start)
+        elif mode == 'date':
+            start, stop = self.date_pos[index:index+2]
+            buffer.seek(self.date_pos[index])
+        act_b = buffer.read(stop-start)
+        text = act_b.decode(codec)[2:-74]
+        return text
+
+class MyReader_iter():
+    def __init__(self, file):
+        self.__file = file
+        self.__file_size = file.seek(0, 2)
+        self.__state = 0
+        self.date_pos = []
+        self.dates = {}
+        self.doc_pos = []
+    def find_docs(self, pattern_date, pattern_start, pattern_end):
+        self.__file.seek(0)
+        last_position = -1
+        while True:
+            line = self.__file.readline()
+            current_position = self.__file.tell()
+            self.__state = current_position/self.__file_size
+            if last_position == current_position:
+                break
+            else:
+                last_position = current_position
+            if re.match(pattern_date, line):
+                self.date_pos.append(current_position)
+                self.dates.setdefault(
+                    self.__file.readline()[:-1], []
+                ).append(len(self.date_pos)-1)
+                yield None
+            elif re.match(pattern_start, line):
+                start_pos = current_position
+                continue
+            elif re.match(pattern_end, line):
+                end_pos = current_position
+                self.doc_pos.append((start_pos, end_pos))
+                continue
+    def find_doc(self, index, mode='act', codec='cp1251'):
+        self.__file.seek(0)
+        buffer = self.__file.buffer
         if mode == 'act':
             start, stop = self.doc_pos[index]
             buffer.seek(start)
---
+++ tempscripts/prgbar.py
@@ -0,0 +1,42 @@
+#https://stackoverflow.com/questions/7310511/how-to-create-downloading-progress-bar-in-ttk
+#https://stackoverflow.com/questions/24769798/tkinter-progressbar-linked-to-function
+#https://stackoverflow.com/questions/24764575/using-the-same-progressbar-in-tkinter-for-several-computation
+
+import tkinter as tk
+from tkinter import ttk
+
+#from tempscripts import iotext as it
+
+#fp = r'C:\Users\EA-ShevchenkoIS\AppData\Local\Continuum\anaconda3\ProjectX\2018-0725-0803_174_info.txt'
+#mr = it.MyReader(open(fp, mode='r'))
+
+class SampleApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.button = ttk.Button(text="start", command=self.start)
+        self.button.pack()
+        self.progress = ttk.Progressbar(self, orient="horizontal",
+                                        length=200, mode="determinate")
+        self.progress.pack()
+
+        self.bytes = 0
+        self.maxbytes = 0
+
+    def start(self):
+        self.update()
+        self.progress["value"] = 0
+        self.maxbytes = 50000
+        self.progress["maximum"] = self.maxbytes
+        self.read_bytes()
+
+    def read_bytes(self):
+        '''simulate reading 500 bytes; update progress bar'''
+        self.bytes += 500
+        self.progress["value"] = self.bytes
+        if self.bytes < self.maxbytes:
+            # read more bytes after 100 ms
+            self.after(100, self.read_bytes)
+
+app = SampleApp()
+app.mainloop()
