9e50a200ed3b42641fd631022c8e9cb0ea66702b
Igor Shevchenko <igorshvch@gmail.com>
2018-12-17 14:10:19 +0300

    Update

--- tempscripts/iotext.py
+++ tempscripts/iotext.py
@@ -6,19 +6,20 @@ from debugger import timer
 class MyReader():
     def __init__(self, file):
         self.__file = file
-        self.__file_size = file.seek(0, 2)
+        self.file_size = file.seek(0, 2)
         self.__state = 0
         self.date_pos = []
         self.dates = {}
         self.doc_pos = []
     @timer
     def find_docs(self, pattern_date, pattern_start, pattern_end):
+        #Patterns: (r'Когда получен\n', r'Текст документа\n', r'-{66}')
         self.__file.seek(0)
         last_position = -1
         while True:
             line = self.__file.readline()
             current_position = self.__file.tell()
-            self.__state = current_position/self.__file_size
+            self.__state = current_position/self.file_size
             if re.match(pattern_date, line):
                 self.date_pos.append(current_position)
                 self.dates.setdefault(
@@ -51,39 +52,39 @@ class MyReader():
 
 class MyReader_iter():
     def __init__(self, file):
-        self.__file = file
-        self.__file_size = file.seek(0, 2)
-        self.__state = 0
+        self.file = file
+        self.file_size = file.seek(0, 2)
+        self.state = 0
         self.date_pos = []
         self.dates = {}
         self.doc_pos = []
     def find_docs(self, pattern_date, pattern_start, pattern_end):
-        self.__file.seek(0)
+        self.file.seek(0)
         last_position = -1
         while True:
-            line = self.__file.readline()
-            current_position = self.__file.tell()
-            self.__state = current_position/self.__file_size
-            if last_position == current_position:
-                break
-            else:
-                last_position = current_position
+            line = self.file.readline()
+            current_position = self.file.tell()
+            self.state = current_position
             if re.match(pattern_date, line):
                 self.date_pos.append(current_position)
                 self.dates.setdefault(
-                    self.__file.readline()[:-1], []
+                    self.file.readline()[:-1], []
                 ).append(len(self.date_pos)-1)
                 yield None
             elif re.match(pattern_start, line):
                 start_pos = current_position
-                continue
+                yield None
             elif re.match(pattern_end, line):
                 end_pos = current_position
                 self.doc_pos.append((start_pos, end_pos))
-                continue
+                yield None
+            if last_position == current_position:
+                break
+            else:
+                last_position = current_position
     def find_doc(self, index, mode='act', codec='cp1251'):
-        self.__file.seek(0)
-        buffer = self.__file.buffer
+        self.file.seek(0)
+        buffer = self.file.buffer
         if mode == 'act':
             start, stop = self.doc_pos[index]
             buffer.seek(start)
--- tempscripts/prgbar.py
+++ tempscripts/prgbar.py
@@ -5,10 +5,10 @@
 import tkinter as tk
 from tkinter import ttk
 
-#from tempscripts import iotext as it
+from tempscripts import iotext as it
 
-#fp = r'C:\Users\EA-ShevchenkoIS\AppData\Local\Continuum\anaconda3\ProjectX\2018-0725-0803_174_info.txt'
-#mr = it.MyReader(open(fp, mode='r'))
+fp = r'C:\Users\EA-ShevchenkoIS\AppData\Local\Continuum\anaconda3\ProjectX\2018-0725-0803_174_info.txt'
+mr = it.MyReader_iter(open(fp, mode='r'))
 
 class SampleApp(tk.Tk):
 
@@ -25,17 +25,26 @@ class SampleApp(tk.Tk):
 
     def start(self):
         self.progress["value"] = 0
-        self.maxbytes = 50000
+        self.hold_obj = mr
+        self.maxbytes = self.hold_obj.file_size
         self.progress["maximum"] = self.maxbytes
+        self.process = self.hold_obj.find_docs(r'Когда получен\n', r'Текст документа\n', r'-{66}')
         self.read_bytes()
 
     def read_bytes(self):
         '''simulate reading 500 bytes; update progress bar'''
-        self.bytes += 500
-        self.progress["value"] = self.bytes
-        if self.bytes < self.maxbytes:
-            # read more bytes after 100 ms
-            self.after(100, self.read_bytes)
+        while self.bytes < self.maxbytes:
+            next(self.process)
+            self.bytes += self.hold_obj.state
+            self.progress["value"] = self.bytes
+            self.update()
+
+        #next(self.process)
+        #self.bytes += self.hold_obj.state
+        #self.progress["value"] = self.bytes
+        #if self.bytes < self.maxbytes:
+        #    # read more bytes after 100 ms
+        #    self.after(100, self.read_bytes)
 
 app = SampleApp()
 app.mainloop()
