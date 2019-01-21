74c7254006491b3c5d703da0995129dd369e300a
Igor Shevchenko <igorshvch@gmail.com>
2019-01-21 18:07:44 +0300

    Update

--- tempscripts/simplegui.py
+++ tempscripts/simplegui.py
@@ -1,3 +1,6 @@
+#Treeview issue:
+#https://stackoverflow.com/questions/49715456/forcing-a-tkinter-ttk-treeview-widget-to-resize-after-shrinking-its-column-width
+
 import tkinter as tk
 from tkinter import ttk
 
@@ -6,3 +9,25 @@ class MyGui(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
         self.mainframe = ttk.Frame(self)
         self.mainframe.grid()
+
+def f(items):
+    cols=['one', 'two', 'three', 'four']
+    root = tk.Tk()
+    tv = ttk.Treeview(root, columns=cols)
+    tv2 = ttk.Treeview(root, columns=cols)
+    for i in cols:
+        tv.heading(i, text=str(i))
+        tv.column(i, width=40, stretch=False)
+    for i in range(len(items)):
+        tv.insert('', 'end', values=(items[i],))
+    tv2.heading('#0', text='HSBGCN')
+    tv2.column('#0', width=60, stretch=False)
+    for i in cols:
+        tv2.heading(i, text=str(i))
+        tv2.column(i, width=40, stretch=False)
+    for i in range(len(items)):
+        tv2.insert('', 'end', text=items[i], values=('',))
+    tv.configure(show='headings')
+    tv.pack()
+    tv2.pack()
+    root.mainloop()
