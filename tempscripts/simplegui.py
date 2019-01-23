86c957c8a046efef9cbb63045e053fbaa41330bd
Igor Shevchenko <igorshvch@gmail.com>
2019-01-23 18:23:57 +0300

    Update

--- tempscripts/simplegui.py
+++ tempscripts/simplegui.py
@@ -4,61 +4,146 @@
 import tkinter as tk
 from tkinter import ttk
 
-class MyGui(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        self.mainframe = ttk.Frame(self)
-        self.mainframe.grid()
+from guidialogs import ffp, fdp
 
-def f(items):
-    cols=['one', 'two', 'three', 'four']
-    root = tk.Tk()
-    tv = ttk.Treeview(root, columns=cols)
-    tv2 = ttk.Treeview(root, columns=cols)
-    for i in cols:
-        tv.heading(i, text=str(i))
-        tv.column(i, width=40, stretch=False)
-    for i in range(len(items)):
-        tv.insert('', 'end', values=(items[i],))
-    tv2.heading('#0', text='HSBGCN')
-    tv2.column('#0', width=60, stretch=False)
-    for i in cols:
-        tv2.heading(i, text=str(i))
-        tv2.column(i, width=40, stretch=False)
-    for i in range(len(items)):
-        tv2.insert('', 'end', text=items[i], values=('',))
-    tv.configure(show='headings')
-    tv.pack()
-    tv2.pack()
-    root.mainloop()
+class TreeviewBuilder():
+    def __init__(self, root=None):
+        self.root = root if root else tk.Tk()
+        self.info = None
+        self.tv = None
+        self.btn1 = None
+        self.data = None
+        self.sort_flag_0 = False
+        self.sort_flag_1 = False
+        self.sort_flag_2 = False
+    
+    def btn1_cmd(self):
+        #print(self.tv.selection())
+        if not self.tv.selection():
+            return None
+        for i in self.tv.selection():
+            print(self.tv.item(i))
 
-def f2(dct):
-    dct = list(dct.items())
-    def button_cmd():
-        print(tv.selection())
-    def inner_f(x):
-        region = tv.identify('region', x.x, x.y)
+    def define(self, event):
+        region = self.tv.identify('region', event.x, event.y)
         if region == 'heading':
-            col = tv.identify('column', x.x, x.y)
-            print(col)
-    def fill_in(column):
-        pass        
-    cols=['class', 'facts']
-    root = tk.Tk()
-    btn1 = ttk.Button(root, text='Print selected', command=button_cmd)
-    tv = ttk.Treeview(root, columns=cols)
-    tv.heading('#0', text='concls')
-    tv.column('#0', width=100, stretch=True)
-    for i in cols:
-        tv.heading(i, text=str(i))
-        tv.column(i, width=40, stretch=True)
-    for pair in dct:
-        for key, val in pair:
-           tv.insert('', 'end', text=val, values=key)
-    tv.bind('<Double-3>', lambda x: inner_f(x))
-    #tv.bind('<Double-1>', lambda x: print(tv.item(tv.identify('item', x.x, x.y))))
-    #tv.bind('<Double-2>', lambda x: print(tv.get_children('')))
-    #tv.bind('<<TreeviewSelect>>', lambda x: print('Select!'))
-    btn1.pack(fill='x', expand='yes')
-    tv.pack(fill='x', expand='yes')
-    root.mainloop()
+            col = self.tv.identify('column', event.x, event.y)
+            return col
+    
+    def sort0(self):
+        self.tv.delete(*self.tv.get_children())
+        self.data = sorted(self.data, key=lambda x: x[0], reverse=self.sort_flag_0)
+        self.sort_flag_0 = not self.sort_flag_0
+        for triple in self.data:
+            key, val1, val2 = triple
+            self.tv.insert(
+                '',
+                'end',
+                text=key,
+                values=(val1, val2)
+            )
+    
+    def sort1(self):
+        self.tv.delete(*self.tv.get_children())
+        self.data = sorted(self.data, key=lambda x: x[1], reverse=self.sort_flag_1)
+        self.sort_flag_1 = not self.sort_flag_1
+        for triple in self.data:
+            key, val1, val2 = triple
+            self.tv.insert(
+                '',
+                'end',
+                text=key,
+                values=(val1, val2)
+            )
+    
+    def sort2(self):
+        self.tv.delete(*self.tv.get_children())
+        self.data = sorted(self.data, key=lambda x: x[2], reverse=self.sort_flag_2)
+        self.sort_flag_2 = not self.sort_flag_2
+        for triple in self.data:
+            key, val1, val2 = triple
+            self.tv.insert(
+                '',
+                'end',
+                text=key,
+                values=(val1, val2)
+            )
+
+    def build_Treeview(self, head, cols, data):
+        self.data = data
+        self.btn1 = ttk.Button(self.root, text='Print selected', command=self.btn1_cmd)
+        self.tv = ttk.Treeview(self.root, columns=cols)
+        self.tv.heading('#0', text=head, command=self.sort0)
+        self.tv.column('#0', width=100, stretch=True)
+        for i, cmd in zip(cols, (self.sort1, self.sort2)):
+            self.tv.heading(i, text=str(i), command=cmd)
+            self.tv.column(i, width=40, stretch=True)
+        for triple in data:
+            key, val1, val2 = triple
+            self.tv.insert(
+                '',
+                'end',
+                text=key,
+                values=(val1, val2)
+            )
+        #tv.bind('<Double-3>', lambda x: inner_f(x))
+        #tv.bind('<Double-1>', lambda x: print(tv.item(tv.identify('item', x.x, x.y))))
+        #tv.bind('<Double-2>', lambda x: print(tv.get_children('')))
+        #tv.bind('<<TreeviewSelect>>', lambda x: print('Select!'))
+        self.tv.bind(
+            '<Double-1>',
+            lambda x: print(self.tv.item(self.tv.identify('item', x.x, x.y)))
+        )
+    
+    def start_widget(self):
+        self.btn1.pack(fill='x', expand='yes')
+        self.tv.pack(fill='x', expand='yes')
+        self.root.mainloop()
+
+
+class Filesloader():
+    def __init__(self, root=None):
+        self.root = root if root else tk.Tk()
+        self.st_var = tk.StringVar()
+        self.btn1 = None
+        self.btn2 = None
+        self.inf_label = None
+    
+    def btn1_cmd(self):
+        self.path = ffp()
+        self.st_var.set(self.path)
+        self.btn1.configure(state='disabled')
+        #self.inf_label.update()
+    
+    def btn2_cmd(self):
+        for btn in self.btn1, self.btn2:
+            btn.configure(state='normal')
+
+    def build_Loader(self):
+        self.btn1 = ttk.Button(
+            self.root,
+            text='Подгрузить данные о файле',
+            command=self.btn1_cmd
+        )            
+        self.btn2 = ttk.Button(
+            self.root,
+            text='Enable state!',
+            command=self.btn2_cmd
+        )
+        self.inf_label = tk.Text(
+            self.root,
+            textvariable=self.st_var,
+            background='pink',
+            width=40
+        )
+    
+    def start_widget(self):
+        self.btn1.pack(fill='x', expand='yes')
+        self.inf_label.pack(fill='x', expand='yes')
+        self.btn2.pack(fill='x', expand='yes')
+        self.root.mainloop()
+
+
+
+
+
