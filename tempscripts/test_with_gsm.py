daac856691d625b47a02b3235247acf4d887a613
Igor Shevchenko <igorshvch@gmail.com>
2019-03-01 17:58:52 +0300

    Update gensim testing module

--- tempscripts/test_with_gsm.py
+++ tempscripts/test_with_gsm.py
@@ -13,6 +13,13 @@ logging.basicConfig(
     style='{', level=logging.INFO
 )
 
+#Preliminaries
+#   corpus_iterator - 'iot.MyReaderPre' object
+#   dct - 'gensim.corpora.Dictionary' object, corresponds to bag-of-words model
+#   pkl - 'tpt.IOPickler' object connected to file with stored BOW VSM
+#   tfidf - 'gensim.models.TfidfModel' object
+#   pkl_tfidf - 'tpt.IOPickler' object connected to file with stored TFIDF VSM
+
 @timer_with_func_name
 def create_dct(file, stpw, no_below, no_above, doc_delimiter=r'-{66}'):
     corpus_iterator = iot.MyReaderPre(file)
@@ -25,7 +32,31 @@ def create_dct(file, stpw, no_below, no_above, doc_delimiter=r'-{66}'):
     dct.compactify()
     return dct, corpus_iterator
 
-def save_model_on_disk(file_pkl, corpus_iterator, dct):
-    pkl = tpt.IOPickler(file_pkl)
+@timer_with_func_name
+def save_vsm_model_on_disk(file_pkl_1, corpus_iterator, dct):
+    pkl = tpt.IOPickler(file_pkl_1)
     pkl.write(dct.doc2bow(doc) for doc in iot.Tokenizer(corpus_iterator))
     return pkl
+
+@timer_with_func_name
+def transform_to_tfidf(file_pkl_2, pkl):
+    tfidf = gsm.models.TfidfModel(pkl.load_all_items())
+    pkl_tfidf = tpt.IOPickler(file_pkl_2)
+    pkl_tfidf.write(tfidf[doc] for doc in pkl.load_all_items())
+    return tfidf, pkl_tfidf
+
+@timer_with_func_name
+def find_key_words(num_kwords, vector, dct):
+    top_tokens = sorted(
+        vector, key=lambda x: x[1], reverse=True
+    )[:num_kwords]
+    head = '{:->20s}     {:->5s}     {:->8s}'.format('KEY', 'ID', 'SCORE')
+    print(head)
+    print('-'*len(head))
+    for token, score in top_tokens:
+        for key, val in dct.token2id.items():
+            if val == token:
+                print(
+                    '{: >20s}     {: >5d}     {: >8.6f}'.format(key,val,score)
+                )
+                break
