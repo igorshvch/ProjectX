58c3eaebe1c0f14630cbe5a5a35301296f91396d
Igor Shevchenko <igorshvch@gmail.com>
2019-08-30 20:50:57 +0300

    New corpus indexer. Util uses .csv files from ConsPlus

--- tempscripts/codeutils.py
+++ tempscripts/codeutils.py
@@ -1,3 +1,5 @@
+import re
+
 from textproc import rwtools
 
 def code_count_lines(folder, suffix='.py', encoding='utf'):
@@ -11,3 +13,47 @@ def code_count_lines(folder, suffix='.py', encoding='utf'):
         except:
             print(i.name)
     return '{: >5d} lines in {: >3d} files'.format(counter, len(fp))
+
+def code_count_lines2(folder, suffix='.py', encoding='utf', delim='\n'):
+    fp = rwtools.collect_exist_files(folder, suffix=suffix)
+    counter_code_lines = 0
+    counter_all_lines = 0
+    counter_blank_lines = 0
+    counter_comment_lines = 0
+    for i in fp:
+        try:
+            with open(i, mode='r', encoding=encoding) as f:
+                code = f.read()
+        except:
+            print(i.name)
+            continue
+        code = code.split('\n')
+        for line in code:
+            line
+            if not line:
+                counter_blank_lines += 1
+            else:
+                line = line.strip(' \t')
+                if not line:
+                    counter_blank_lines += 1
+                elif line[0] == '#':
+                    counter_comment_lines += 1
+                else:
+                    counter_code_lines += 1
+        counter_all_lines = (
+            counter_blank_lines
+            + counter_code_lines
+            + counter_comment_lines
+        )   
+    return (
+        delim.join(
+            (
+                'Total lines: {: >6d} in {: >3d} files'.format(
+                    counter_all_lines, len(fp)
+                ),
+                '\tcode lines:\t{: >6d}'.format(counter_code_lines),
+                '\tcomment lines:\t{: >6d}'.format(counter_comment_lines),
+                '\tblank lines:\t{: >6d}'.format(counter_blank_lines)
+            )
+        )
+    )
--- tempscripts/tmp10.py
+++ tempscripts/tmp10.py
@@ -1,3 +1,4 @@
+import sys
 import re
 import csv
 from datetime import date
@@ -22,15 +23,23 @@ def string_to_date(string):
 data_trans_funcs = {
     'comas_to_newlines': lambda x: re.subn(r',(?=[\w\n-])', '\n', x)[0],
     'find_act_content': lambda x: re.search(
-        r'(?<=\bустановил\n).*(?=\bПредседательствующий\n)',
-        x
+        r'(?<=\nустановил:\n).*(?=\nПредседательствующий|\nПредседательствующий судья)',
+        x,
+        flags=re.DOTALL
     ).group(),
     'string_to_date': lambda x: string_to_date(x),
     'separate_act_ids': lambda x: set(x.split(',')),
-    'court': lambda x: x.split('_')[0]
+    'court': lambda x: x.split('_')[0],
+    'sinopsis': lambda x: re.split(r',(?=[\w\n-])', x, maxsplit=1)
 }
 
 def custom_processor_for_ConsPlus_data(dct):
+    req_name = dct['Название документа']
+    if req_name[:6] != 'Постан':
+        return None
+    name, sinopsis = data_trans_funcs['sinopsis'](req_name)
+    dct['Название документа'] = name
+    dct['Описание'] = sinopsis
     text = dct['Текст документа']
     comas_to_newlines = data_trans_funcs['comas_to_newlines'](text)
     dct['Текст документа'] = comas_to_newlines
@@ -42,25 +51,75 @@ def custom_processor_for_ConsPlus_data(dct):
     date_loading = dct['Когда получен']
     dct['Когда получен'] = data_trans_funcs['string_to_date'](date_loading)
     date_issue = dct['Дата']
-    dct['Дата'] = None ########
-
-
-
-
-    cut_edges = reg_exp_funcs['search'](comas_to_newlines)
-    dct[key_to_transform] = comas_to_newlines
-    if cut_edges:
-        dct['Содержательная часть'] = cut_edges.group()
-    else:
-        dct['Содержательная часть'] = None
+    dct['Дата'] = data_trans_funcs['string_to_date'](date_issue)
+    req_num = dct['Номер']
+    dct['Номер'] = data_trans_funcs['separate_act_ids'](req_num)
+    try:
+        req_court = dct['Принявший орган']
+    except:
+        req_court = dct['Судья']
+    dct['Принявший орган'] = data_trans_funcs['court'](req_court)
     return dct
 
-def inspect_corpus(file, iop_obj, process_func=False):
+def inspect_corpus(file,
+                   iop_obj=iop.IOPickler(),
+                   process_func=custom_processor_for_ConsPlus_data):
     reader = csv.DictReader(file, delimiter=';')
     for row in reader:
         if process_func:
             row = process_func(row)
+            if not row:
+                continue
         iop_obj.append(row)
+    return iop_obj
 
 
+if __name__ == '__main__':
+    mode = sys.argv[1]
+    if mode == '-s':
+        from guidialogs import ffp
+        with open(ffp(), mode='r', newline='') as f:
+            print(f.name)
+            iop_obj = inspect_corpus(f)
+        row = iop_obj[0]
+        print('-'*101)
+        print('{: >30s}|{: <50s}|{: >19s}'.format('Key', 'Data', 'Type'))
+        print('-'*101)
+        for key in row.keys():
+            data = row[key]
+            t = type(data)
+            data = str(data).replace('\n', '##')
+            if len(data)>45:
+                data = data[:45]+'...'
+            print('{: >30s}|{: <50s}|{: >19s}'.format(str(key)[:30], data, str(t)))
+        print('-'*101)
+    elif mode == '-p':
+        from guidialogs import fdp
+        from textproc import collect_exist_files
+        from time import time
+        fp = collect_exist_files(fdp(), suffix='.csv')
+        time1 = time()
+        for ind, path in enumerate(fp):
+            with open(path, mode='r', newline='') as f:
+                print(ind, 'chek:', path.stem, end=' ')
+                iop_obj = inspect_corpus(f)
+                row = iop_obj[0]
+                for key in row.keys():
+                    data = row[key]
+                    t = type(data)
+                    data = str(data).replace('\n', '##')
+                    if len(data)>45:
+                        data = data[:45]+'...'
+                print('OK')
+        time2 = time()
+        time_res = time2-time1
+        print('time: {: >7.3f} min ({: >7.3f} sec)'.format(
+            time_res/60, time_res)
+        )
+
+    else:
+        print(
+            'Mode arg error! Please type correct mode arg:',
+            '-s for single mode, -p for pipline mode'
+        )
 
