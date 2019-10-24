61d0e7082e6bdc79766e71d632723cf1dcb2831f
Igor Shevchenko <igorshvch@gmail.com>
2019-10-24 17:32:11 +0300

    EC experiment

--- tempscripts/tmp9.py
+++ tempscripts/tmp9.py
@@ -1,7 +1,9 @@
 import re
 import random
+from collections import Counter
 
 import debugger as dbg
+from writer import writer
 
 @dbg.timer_with_func_name
 def find_demands(corp_iter):
@@ -49,35 +51,38 @@ def create_art_map(corp_iter, iop_object, pat='pat4', delim=10000):
     '''
     time = dbg.time()
     pattern0 = (
-        '[А-я.]+ [0-9]{1,4}\.*[0-9]{0,4} '
-        +'[А-я.]* [0-9]{1,4}\.*[0-9]{0,4} '
-        +'[А-я.]+ [А-я.]+'
+        r'[А-я.]+ [0-9]{1,4}\.*[0-9]{0,4} '
+        +r'[А-я.]* [0-9]{1,4}\.*[0-9]{0,4} '
+        +r'[А-я.]+ [А-я.]+'
     )
     pattern1 = (
-        '[А-я.]+ [0-9]{1,4}\.*[0-9]{0,4} *'
-        +'[А-я.]* *[0-9]{1,4}\.*[0-9]{0,4} *'
-        +'[А-я.]+ [А-я.]+'
+        r'[А-я.]+ [0-9]{1,4}\.*[0-9]{0,4} *'
+        +r'[А-я.]* *[0-9]{1,4}\.*[0-9]{0,4} *'
+        +r'[А-я.]+ [А-я.]+'
     )
     pattern2 = (
-        '[А-я.]+ [0-9]{1,4}\.*[0-9]{0,4} *'
-        +'[А-я.]* *[0-9]{1,4}\.*[0-9]{0,4},* *'
-        +'[0-9]{0,4}\.*[0-9]{0,4} *'
-        +'[А-я.]+ [А-я.]+'
+        r'[А-я.]+ [0-9]{1,4}\.*[0-9]{0,4} *'
+        +r'[А-я.]* *[0-9]{1,4}\.*[0-9]{0,4},* *'
+        +r'[0-9]{0,4}\.*[0-9]{0,4} *'
+        +r'[А-я.]+ [А-я.]+'
     )
     pattern3 = (
-        '[А-я]*[,.:;]* *[А-я0-9.]*[,.:;]* *[А-я]*[,.:;]* *[А-я0-9.]*[,.:;]* *'
-        +'[Сс]т[.атьяиеюёймх]{1,6} [0-9]{1,4}\.*[0-9]{0,4}\W* *'
-        +'[0-9]{0,4}\.*[0-9]{0,4}\W* *[0-9]{0,4}\.*[0-9]{0,4}\W* '
-        +'[А-я]+ [А-я]* *\w+ [А-я0-9\-ФКЗ]*'
+        r'[А-я]*[,.:;]* *[А-я0-9.]*[,.:;]* *[А-я]*[,.:;]* *[А-я0-9.]*[,.:;]* *'
+        +r'[Сс]т[.атьяиеюёймх]{1,6} [0-9]{1,4}\.*[0-9]{0,4}\W* *'
+        +r'[0-9]{0,4}\.*[0-9]{0,4}\W* *[0-9]{0,4}\.*[0-9]{0,4}\W* '
+        +r'[А-я]+ [А-я]* *\w+ [А-я0-9\-ФКЗ]*'
     )
+    ####Following patterns give best results on EC court practice.
     pattern4 = (
-        '[А-я0-9N\-.]*\W{0,2}[А-я0-9N\-.]+\W{1,2}[Сс]т[а.]\w* '
-        +'[0-9]{1,4}\.*[0-9]{0,4}\W{1,2}[А-я0-9N\-]+\W{1,2}'
-        +'[А-я0-9N\-]+\W{1,2}[А-я0-9N\-]+\W{1,2}[А-я0-9N\-]+\W{1,2}'
+        r'[А-я0-9N\-.]*\W{0,2}[А-я0-9N\-.]+\W{1,2}[Сс]т[а.]\w* '
+        +r'[0-9]{1,4}\.*[0-9]{0,4}\W{1,2}[А-я0-9N\-]+\W{1,2}'
+        +r'[А-я0-9N\-]+\W{1,2}[А-я0-9N\-]+\W{1,2}[А-я0-9N\-]+\W{1,2}'
     )
+    ####Pattern # 5 is slightly more accurate but takes more time to compute
     pattern5 = (
-        '[А-я0-9N\-.]*\W{0,2}[А-я0-9N\-.]+\W{1,2}[Сс]т[а.]\w* [0-9]{1,4}\.*[0-9]{0,4}\W{1,2}[А-я0-9N\-]+\W{1,2}[А-я0-9N\-]+\W{1,2}[А-я0-9N\-]*\W{0,2}[А-я0-9N\-]*\W{0,2}'
+        r'[А-я0-9N\-.]*\W{0,2}[А-я0-9N\-.]+\W{1,2}[Сс]т[а.]\w* [0-9]{1,4}\.*[0-9]{0,4}\W{1,2}[А-я0-9N\-]+\W{1,2}[А-я0-9N\-]+\W{1,2}[А-я0-9N\-]*\W{0,2}[А-я0-9N\-]*\W{0,2}'
     )
+    ####
     options = {
         'pat0': pattern0,
         'pat1': pattern1,
@@ -104,7 +109,149 @@ def create_art_map(corp_iter, iop_object, pat='pat4', delim=10000):
                     break
         iop_object.append(cleaned)
     
+def create_parser(parser, number='sing'):
+    '''
+    Creating morphological analyzer to incline words.
+    Two optiona re availble:
+    - incline singular word form
+    - incline plural word form
+    '''
+    def inner_sing(word):
+        res = []
+        lex_res = parser(word).lexeme
+        for lex_mode in lex_res:
+            if 'sing' in lex_mode.tag:
+                res.append(lex_mode[0])
+        return res
+    def inner_plur(word):
+        res = []
+        lex_res = parser(word).lexeme
+        for lex_mode in lex_res:
+            if 'plur' in lex_mode.tag:
+                res.append(lex_mode[0])
+        return res
+    options = {
+        'sing': inner_sing,
+        'plur': inner_plur
+    }
+    return options[number]
+
+def collect_art_dispers(counter, lng, verbose=False):
+    '''
+    Returns:
+    [
+        #Article | % of all articles
+        ('213', 0.3333333333333333),
+        ('11', 0.16666666666666666),
+        ('22', 0.16666666666666666),
+        ('76', 0.16666666666666666),
+        ('212', 0.16666666666666666)
+    ]
+    '''
+    holder = []
+    for key, val in counter.most_common():
+        holder.append((key, val/lng))
+    if verbose:
+        for key, val in holder:
+            print('key: {: >4s} - {: >5.2f}%'. format(key, val))
+    return holder
+
+#################
+#A set of functions for arcticle extractions and law names analysis
+def convert_list_of_strings_to_list_of_tokens(list_of_strings):
+    return [item.lower().split(' ') for item in list_of_strings]
     
+def extract_arts(list_of_l_tokens, test_set):
+    '''
+    Exctracts numbers of law articles from list of tokens:
+        ['some', 'text', '123', 'civil', 'code']
+        return: '123 civil code'
+    Also do some simple statistical calculations
+    '''
+    res = []
+    for row in list_of_l_tokens:
+        for ind, _ in enumerate(row):
+            token = row[ind]
+            if token in test_set and ind > 0:
+                res.append(row[ind-1])
+    count_res = Counter(res)
+    percents = collect_art_dispers(count_res, len(res))
+    return count_res, res, percents
+
+def extract_arts_main(list_of_strings, test_set):
+    '''
+    Main function in the set
+    '''
+    list_of_lists = convert_list_of_strings_to_list_of_tokens(list_of_strings)
+    count_res, res, percents = extract_arts(list_of_lists, test_set)
+    return count_res, res, percents
+######
+def extract_new_laws(list_of_l_tokens, test_set):
+    '''
+    Variation of the 'extract_arts' function. This function
+    extracts some text information (in particulary, law names)
+    which occures after article number:
+        ['some', 'text', '123', 'civil', 'code']
+        return: 'civil code'
+    '''
+    res = []
+    for row in list_of_l_tokens:
+        for ind, _ in enumerate(row):
+            token = row[ind]
+            if token in test_set and ind > 0:
+                res.append(' '.join(row[ind:]))
+                break
+    #count_res = Counter(res)
+    #percents = collect_art_dispers(count_res, len(res))
+    return res #count_res, res, percents
+
+def extract_new_laws_main(list_of_strings, test_set):
+    '''
+    Main function in this subdomain
+    '''
+    list_of_lists = convert_list_of_strings_to_list_of_tokens(list_of_strings)
+    res = extract_new_laws(list_of_lists, test_set)
+    return res
+#################
+
+def docs_writer(nums, doc_keeper):
+    '''
+    Write documents from doc_keeper (iop_object of corpus iterator)
+    to .txt files. File names are created in respect to document numbers
+    '''
+    for num in nums:
+        writer(doc_keeper(num), 'test_{}'.format(num), mode='w')
+
+#################
+
+def list_of_lists_2_list_of_strings(list_of_lists):
+    return [string for column in list_of_lists for string in column]
+
+def create_lem_map_for_list_of_strings(list_of_strings, parser):
+    long_string = ' '.join(list_of_strings)
+    tokens = set(re.findall(r'[А-я0-9n\-]+', long_string))
+    lem_map = {token:parser(token).normal_form for token in tokens}
+    return lem_map
+
+def normalize_strings_in_lists_of_strings(list_of_strings, lem_map):
+    list_of_lists_of_tokens = [
+        re.findall(r'[А-я0-9n\-]+', raw_str) for raw_str in list_of_strings
+    ]
+    list_of_lists_of_lemms = [
+        [lem_map[token] for token in lst] for lst in list_of_lists_of_tokens
+    ]
+    list_of_lem_strings = [' '.join(row) for row in list_of_lists_of_lemms]
+    return list_of_lem_strings
+
+def main_normalize_strings_in_lists(list_of_lists, parser):
+    print('start')
+    list_of_strings = list_of_lists_2_list_of_strings(list_of_lists)
+    print('\tcreated list_of_strings, length:', len(list_of_strings))
+    lem_map = create_lem_map_for_list_of_strings(list_of_strings, parser)
+    print('\tcreated lem_map, length:', len(lem_map))
+    return normalize_strings_in_lists_of_strings(list_of_strings, lem_map)
+
+
 #pattern2 = (
 #'[А-я]*[,.:;]* *[А-я0-9.]*[,.:;]* *[А-я]*[,.:;]* *[А-я0-9.]*[,.:;]* *[Сс]т[.атьяиеюёймх]{1,6} [0-9]{1,4}\.*[0-9]{0,4}\W* *[0-9]{0,4}\.*[0-9]{0,4}\W* *[0-9]{0,4}\.*[0-9]{0,4}\W* [А-я]+ [А-я]* *\w+ [А-я0-9\-ФКЗ]*'
 #)
